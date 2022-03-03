40fc396cf0683cd3818aaf76b68758df7cb03dbe
Michael Z <michaelzwierzanski@gmail.com>
2022-03-03 12:33:49 +0000

    sequelize.close() added app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ const app = async (yargsObj) => {
             console.log(JSON.stringify(await listMovies(), null, 2));
         } else if (yargsObj.list) {
             console.log(JSON.stringify(await listMovies({[yargsObj.key]: yargsObj.value}), null, 2))
+        } else {
+            console.log("Incorrect command");
         }
+        await sequelize.close();
     } catch (error) {
         console.log(error);
     }
